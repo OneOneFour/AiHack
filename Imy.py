34cb7ad2041e1c51fd5f88da1da1d39a2363cd9e
Imy Younge <imy.younge@sky.com>
2020-02-29 20:51:02 +0000

    Finally something plots

--- Imy.py
+++ Imy.py
@@ -1,20 +1,34 @@
+
 from nhs import Session
 from nhs.models import Prescription
 import numpy as np
 import pandas as pd
-
 import pandas as pd
 import numpy as np
 #from plotnine import *
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pres = Prescription.get_dataframe()
 print(pres.head())
+pres['bnf_code_id'] = pres['bnf_code_id'].astype(str)
+#fours =  # find the number of prescriptions beginning with a 4 for each time period
 
-# find the number of prescriptions beginning with a 4 for each time period
 
-# set figure margins
-plt.figure(figsize=(16, 6))
+#fours['dep_per_person'] = #sum of all per gp / total number of predications
+
 # select colour scheme for the plot
-cmap = sns.diverging_palette(15, 220, as_cmap=True, center="light", s = 99)
+#cmap = sns.diverging_palette(15, 220, as_cmap=True, center="light", s = 99)
 #plot time period on the x axis, sum of depression predictions on the y axis
-sns.boxplot(y=pres['number_of_patients'], x=small["high_level_health_geography1"], palette="Blues")
+#sns.scatterplot(x=pres['date_span'], y=pres['number_of_prescriptions'], palette="Blues")
+#plt.show()
+
+
+# Select all the ones that begine witha four in a column
+
+
+
+
+
+
+
