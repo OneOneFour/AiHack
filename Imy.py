3de6e6f5fce73a940fcd9ff06d46833a1dff6063
Imy Younge <imy.younge@sky.com>
2020-02-29 21:32:56 +0000

    New columns

--- Imy.py
+++ Imy.py
@@ -1,4 +1,3 @@
-
 from nhs import Session
 from nhs.models import Prescription
 import numpy as np
@@ -9,26 +8,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Formatting the pres data
 pres = Prescription.get_dataframe()
-print(pres.head())
-
 pres['bnf_code_id'].to_string()
+pres['dep_per_total'] = (sum(pres[pres['bnf_code_id'].str.startswith('4')])) - sum[pres[pres['bns_code_id']]]
+
+sns.scatterplot(x=pres['date_span'], y=pres['number_of_prescriptions'], palette="Blues")
+plt.show() # plot time period on the x axis, sum of depression predictions on the y axis
+
+### FOURS
+# Creating the fours dataset
 fours = pres[pres['bnf_code_id'].str.startswith('4')]
-print(fours.head())
-
-#fours =  # find the number of prescriptions beginning with a 4 for each time period
-
+fours.shape  # find the number of prescriptions beginning with a 4 for each time period
 
 #fours['dep_per_person'] = #sum of all per gp / total number of predications
 
-# select colour scheme for the plot
-#cmap = sns.diverging_palette(15, 220, as_cmap=True, center="light", s = 99)
-#plot time period on the x axis, sum of depression predictions on the y axis
-#sns.scatterplot(x=pres['date_span'], y=pres['number_of_prescriptions'], palette="Blues")
-#plt.show()
+# Plot the graph for just anti-depressants
+sns.scatterplot(x=fours['date_span'], y=fours['number_of_prescriptions'], palette="Blues")
+plt.show() # plot same graph but just for the fours
 
 
-# Select all the ones that begine witha four in a column
 
 
 
