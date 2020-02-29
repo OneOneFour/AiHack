28197d31248ea528f7c06ef4f947526309af3df8
Robert <robertkng104@gmail.com>
2020-02-29 17:26:39 +0000

    a couple of minor tweaks

--- nhs/simplequery.py
+++ nhs/simplequery.py
@@ -1,3 +1,5 @@
 from . import Session
-from .models import Location
+from .models import Prescription
 
+with Session() as sesh:
+    perscripts = sesh.query(Prescription).filter("0403" in Prescription.bnf_code).all()
