bbec3e4ab4f8a686f25bb732923c7139ebbca057
Robert <robertkng104@gmail.com>
2020-03-01 02:10:15 +0000

    made changes

--- nhs/schemas.py
+++ nhs/schemas.py
@@ -1,10 +1,25 @@
-from .models import Prescription,BNFStem,Location,LocationPatientNumbers
+from .models import Prescription, BNFStem, Location, LocationPatientNumbers
 from . import ma
 
-class PrescriptionSchema(ma.Schema):
-    class Meta:
-        fields = ("number_of_perscriptions","date_span","bnf_code","location")
 
-class BNFStemSchema(ma.Schema):
+class PrescriptionSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ("code_stem","code_name")
+        model = Prescription
+
+
+class BNFStemSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = BNFStem
+        exclude = ("id",)
+
+
+class PrescriptionWithBNF(ma.Schema):
+    class Meta:
+        fields = ("date_span", "number_of_prescriptions", "bnf_code")
+
+    bnf_code = ma.Nested(BNFStemSchema)
+
+
+class LocationSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Location
--- nhs/views.py
+++ nhs/views.py
@@ -1,11 +1,41 @@
 from flask import jsonify
 
-from .schemas import BNFStemSchema
-from . import app, db, BNFStem
+from .schemas import BNFStemSchema, LocationSchema, PrescriptionSchema, PrescriptionWithBNF
+from . import app, db, BNFStem, Location, Prescription
+from datetime import datetime
 
 
-@app.route("/")
-def hello_world():
-    ps_schema = BNFStemSchema()
+@app.route("/api/bnf_stems", methods=["GET"])
+def get_bnf_stems():
+    ps_schema = BNFStemSchema(many=True)
     ps = db.session.query(BNFStem).all()
     return jsonify(ps_schema.dump(ps))
+
+
+@app.route("/api/locations", methods=["GET"])
+def get_locations():
+    location_schema = LocationSchema(many=True)
+    locations = db.session.query(Location).all()
+    return jsonify(location_schema.dump(locations))
+
+
+@app.route("/api/locations/<code>", methods=["GET"])
+def get_location(code):
+    location_schema = LocationSchema()
+    location = db.session.query(Location).filter(Location.gp_code == code).one()
+    return jsonify(location_schema.dump(location))
+
+
+@app.route("/api/locations/<code>/prescriptions", methods=["GET"])
+def get_location_prescriptions(code):
+    prescription_schema = PrescriptionWithBNF(many=True)
+    prescriptions = Prescription.query.filter(Prescription.location.has(gp_code=code))
+    return jsonify(prescription_schema.dump(prescriptions))
+
+
+@app.route("/api/locations/<code>/<int:year>/<int:month>")
+def get_location_prescriptions_in_timeframe(code, year, month):
+    t = datetime(year=year, month=month, day=1)
+    pres = Prescription.query.filter(Prescription.location.has(gp_code=code)).filter(Prescription.date_span == t)
+    prescription_schema = PrescriptionWithBNF(many=True)
+    return jsonify(prescription_schema.dump(pres))
